4ddfc26ca572f7ec52a9208405a715cfc4b8ab73
pujitha-reddy-gth <pujithathammaneni@gmail.com>
2025-05-14 20:13:09 +0530

    Add files via upload

---
+++ EvenNumbers.java
@@ -0,0 +1,13 @@
+package com.blc.elc;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		System.out.print("Even Numbers:");
+		for(int i=2;i<=20;i=i+2) {
+			System.out.print(i +" ");
+		}
+
+	}
+
+}
