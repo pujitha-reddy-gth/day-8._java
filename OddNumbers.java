fda9ec63a543ae1d11aab5fb32e3970dfdb01a80
pujitha-reddy-gth <pujithathammaneni@gmail.com>
2025-05-14 20:13:35 +0530

    Add files via upload

---
+++ OddNumbers.java
@@ -0,0 +1,13 @@
+package com.blc.elc;
+
+public class OddNumbers {
+
+	public static void main(String[] args) {
+		System.out.print("Odd Numbers:");
+		for(int i=5;i<=50;i=i+2) {
+			System.out.print(i + " ");
+		}
+
+	}
+
+}
